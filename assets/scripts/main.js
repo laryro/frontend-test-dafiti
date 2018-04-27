bc6d296eb6be214c1f5f32cc3441519d23ce7f32
Laryssa Rocha <laryssa.vrocha@gmail.com>
2018-04-27 02:03:06 -0300

    get user info template via fetch and uses store.js to get localstorage

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,23 +1,37 @@
 class App {
-	
+
 	constructor() {
 		this.templatePath = 'assets/templates/';
-		//the idea here is to use fetch to get the template from path 'assets/templates', but for now i'll keep it inline
-		this.userProfile = `
-			<p>Hello, <strong>{{userName}}</strong></p>
-			{{#age}}<p><strong>Age:</strong> {{age}}</p>{{/age}}
-			{{#email}}<p><strong>E-mail:</strong> {{email}}</p>{{/email}}
-			{{#location}}<p><strong>Location:</strong> {{location}}</p>{{/location}}
-			`;
 
+		this.init();
+		this.populateUserProfile();
 	}
-	
-	populateUserProfile(userInfo) {
-		if (!userInfo) {
-			console.warn('There was a problem setting user info');
+
+	init() {
+		
+	}
+
+	async populateUserProfile() {
+		let user = store.get('user');
+
+		if (!user) {
 			return false;
 		}
 
+		const templateName = 'userProfile.html';
+		const template = await this.getTemplate(templateName);
+		const userInfo = Mustache.render(template, user);
+		document.querySelector('.profile-info').innerHTML = userInfo;
+	}
+
+	async getTemplate(templateName = '') {
+		if (templateName === '') {
+			return '';
+		}
+
+		const templateResponse = await fetch(this.templatePath + templateName)
+		const template = await templateResponse.text();
+		return template;
 	}
 
 	updateUserInfo(formData) {
