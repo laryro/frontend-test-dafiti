7147ca9905abe40a7830d1b193855b484b28aebb
Laryssa Rocha <laryssa.vrocha@gmail.com>
2018-04-27 02:48:00 -0300

    add method to update user info

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,16 +3,12 @@ class App {
 	constructor() {
 		this.templatePath = 'assets/templates/';
 
-		this.init();
 		this.populateUserProfile();
-	}
-
-	init() {
-		
+		this.updateUserInfo();
 	}
 
 	async populateUserProfile() {
-		let user = store.get('user');
+		const user = store.get('user');
 
 		if (!user) {
 			return false;
@@ -34,8 +30,40 @@ class App {
 		return template;
 	}
 
-	updateUserInfo(formData) {
+	updateUserInfo() {
+		//sorry about jquery :(
+		$('#change-profile').on('show.bs.modal', (event) => {
+			const user = store.get('user');
+			if (user) {
+				document.querySelector('#form-profile-info #name').value = user.name;
+				document.querySelector('#form-profile-info #age').value = user.age;
+				document.querySelector('#form-profile-info #location').value = user.location;
+				document.querySelector('#form-profile-info #email').value = user.email;
+			}
+		});
 
+		document.querySelector('#form-profile-info').addEventListener('submit', (event) =>{
+			event.preventDefault();
+			const 
+				name = document.querySelector('#form-profile-info #name').value,
+				age = document.querySelector('#form-profile-info #age').value,
+				location = document.querySelector('#form-profile-info #location').value,
+				email = document.querySelector('#form-profile-info #email').value;
+			
+			const userInfo = {
+				'name': name,
+				'age': age,
+				'location': location,
+				'email': email,
+			};
+
+			store.set('user', userInfo);
+			this.populateUserProfile();
+
+			//sorry about jquery :(
+			$('#change-profile').modal('hide');
+
+		});
 	}
 }
 
